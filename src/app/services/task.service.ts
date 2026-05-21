b92ec60bb3ceabbbedc540dff31730132d647f0d
Ravi <goswamiravi076@gmail.com>
2026-05-21 18:16:27 +0530

    fix port for render

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,9 @@
+import { environment } from '@env';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Task } from '../models/task.model';
 import { Page } from '../models/page.model';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
